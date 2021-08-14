3e0cd1ce1118ba9eece7152730e6235f26df3530
Dohyun Shin <shindohyun00@gmail.com>
2021-08-14 23:41:48 +0900

    인터셉터 패키지 변경

--- src/main/java/com/ictsaeil/sample/config/WebConfig.java
+++ src/main/java/com/ictsaeil/sample/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.ictsaeil.sample.interceptor.PageInterceptor;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	private static final List<String> PATH_PATTERNS = Arrays.asList("/*");
--- src/main/java/com/ictsaeil/sample/interceptor/PageInterceptor.java
+++ src/main/java/com/ictsaeil/sample/interceptor/PageInterceptor.java
@@ -1,4 +1,4 @@
-package com.ictsaeil.sample.config;
+package com.ictsaeil.sample.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
