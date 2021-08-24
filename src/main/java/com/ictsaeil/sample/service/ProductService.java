c9a731c81f5460bab2f295881ea800e47553b5b2
Dohyun Shin <shindohyun00@gmail.com>
2021-08-25 00:50:21 +0900

    properties, log4j2 분리하기
    gradle war 빌드 지원 (tiles가 jar 배포 지원 안함)

--- src/main/java/com/ictsaeil/sample/service/ProductService.java
+++ src/main/java/com/ictsaeil/sample/service/ProductService.java
@@ -3,6 +3,8 @@ package com.ictsaeil.sample.service;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import com.ictsaeil.sample.mapper.ProductMapper;
 
 @Service
 public class ProductService {
+	private Logger logger = LogManager.getLogger(ProductService.class);
+	
 	@Autowired
 	ProductMapper productMapper;
 	
@@ -29,6 +33,13 @@ public class ProductService {
 			
 			product.put("STATUS", status);
 		}
+		
+		logger.fatal("[FATAL]");
+		logger.error("[ERROR]");
+		logger.warn("[WARN]");
+		logger.info("[INFO]");
+		logger.debug("[DEBUG]");
+		logger.trace("[TRACE]");
 		
 		return products;
 	}
