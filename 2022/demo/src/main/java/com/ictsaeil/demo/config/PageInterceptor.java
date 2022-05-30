28eb42df05ff8b9a1ed11f37fcf814380a4744fe
dohyun <shindohyunx@gmail.com>
2022-05-30 18:19:49 +0900

    [추가] [Interceptor] 예제 코드

---
+++ 2022/demo/src/main/java/com/ictsaeil/demo/config/PageInterceptor.java
@@ -0,0 +1,37 @@
+package com.ictsaeil.demo.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class PageInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("[preHandle]" + request.getRequestURI());
+		
+		// 마이페이지 이동 요청의 경우 컨트롤러를 실행하지 않고
+		// 로그인 페이지로 redirect 시킨다.
+		if(request.getRequestURI().equals("/my-page")) {
+			response.sendRedirect("/signin");
+			return false;
+		}
+		
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println("[postHandle]" + request.getRequestURI());
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		System.out.println("[afterCompletion]" + request.getRequestURI());
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+	
+}
---
+++ 2022/demo/src/main/java/com/ictsaeil/demo/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.ictsaeil.demo.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+	private static final List<String> PATH_PTTERNS = Arrays.asList("/*");
+	private static final List<String> EX_PATH_PATTERNS = Arrays.asList("/api/*");
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PageInterceptor())
+		.addPathPatterns(PATH_PTTERNS)
+		.excludePathPatterns(EX_PATH_PATTERNS);
+		
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+	
+}
